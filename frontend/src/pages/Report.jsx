2919029c63a9801f62a4757f76a07b9846068195
captainAyan <ayanchakraborty105@gmail.com>
2023-03-06 00:51:11 +0530

    feat: :sparkles: added answer details view in quiz report

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import axios from "axios";
 
 import authConfig from "../util/authConfig";
-import { GET_QUIZ_ANSWERS_URL } from "../constants/api";
+import { GET_QUIZ_ANSWERS_URL, GET_QUIZ_URL } from "../constants/api";
 
 export default function Report() {
   const navigate = useNavigate();
@@ -14,8 +14,10 @@ export default function Report() {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [detailViewId, setDetailViewId] = useState();
 
   const [data, setData] = useState();
+  const [quiz, setQuiz] = useState();
 
   const [searchParams] = useSearchParams();
   const [page, setPage] = useState(parseInt(searchParams.get("page")) || 0);
@@ -25,6 +27,11 @@ export default function Report() {
       .get(`${GET_QUIZ_ANSWERS_URL(id)}?page=${page}`, authConfig(user?.token))
       .then(({ data }) => {
         setData(data);
+        setIsError(false);
+      })
+      .then(() => axios.get(GET_QUIZ_URL + id, authConfig(user?.token)))
+      .then(({ data }) => {
+        setQuiz(data);
         setIsError(false);
       })
       .catch((e) => {
@@ -69,6 +76,11 @@ export default function Report() {
               <td>{answer.marksObtained}</td>
               <td>{answer.user.id}</td>
               <td>{answer.user.email}</td>
+              <td>
+                <a href="#" onClick={() => setDetailViewId(answer.id)}>
+                  View
+                </a>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -79,6 +91,73 @@ export default function Report() {
         <span>Page {page}</span>
         <button onClick={() => setPage(page + 1)}>»</button>
       </div>
+
+      <h3>Answer Details</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Question Id</th>
+            <th>Question Description</th>
+            <th>Question Weightage</th>
+            <th>Question Negative Mark</th>
+            <th>Correct Answer (Id)</th>
+            <th>Examinee Answer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {quiz?.questions?.map((question, i) => (
+            <tr key={question.id}>
+              <td>{question.id}</td>
+              <td>{question.description}</td>
+              <td>{question.weightage}</td>
+              <td>{question.negativeMark}</td>
+              <td>
+                {`${
+                  question.options[
+                    question.options
+                      .map((op) => op.id)
+                      .indexOf(question.correctOptionId)
+                  ].description
+                }
+                  (${
+                    question.options[
+                      question.options
+                        .map((op) => op.id)
+                        .indexOf(question.correctOptionId)
+                    ].id
+                  })`}
+              </td>
+              <td>
+                {`${
+                  question.options[
+                    question.options
+                      .map((op) => op.id)
+                      .indexOf(
+                        data?.answers[
+                          data?.answers?.map((a) => a.id).indexOf(detailViewId)
+                        ]?.answers[i].optionId
+                      )
+                  ]?.description || ""
+                }
+                  (${
+                    question.options[
+                      question.options
+                        .map((op) => op.id)
+                        .indexOf(
+                          data?.answers[
+                            data?.answers
+                              ?.map((a) => a.id)
+                              .indexOf(detailViewId)
+                          ]?.answers[i].optionId
+                        )
+                    ]?.id || " - "
+                  })
+                  `}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
